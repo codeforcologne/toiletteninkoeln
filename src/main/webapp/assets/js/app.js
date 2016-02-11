2a0ae3d89103f47d565a7d1acfeb4c9e2e51a344
wolfram <wolfram@wolframs-mbp-3.fritz.box>
2016-02-11 06:08:08 +0100

    attribution

--- src/main/webapp/assets/js/app.js
+++ src/main/webapp/assets/js/app.js
@@ -271,7 +271,7 @@ var attributionControl = L.control({
 });
 attributionControl.onAdd = function (map) {
   var div = L.DomUtil.create("div", "leaflet-control-attribution");
-  div.innerHTML = "<span class='hidden-xs'>Developed by <a href='http://bryanmcbride.com'>bryanmcbride.com</a> | </span><a href='#' onclick='$(\"#attributionModal\").modal(\"show\"); return false;'>Attribution</a>";
+  div.innerHTML = "<span class='hidden-xs'>Developed by <a href='https://github.com/weberius/toiletteninkoeln' target='_blank'>Wolfram Eberius</a> | </span><a href='#' onclick='$(\"#attributionModal\").modal(\"show\"); return false;'>Attribution</a>";
   return div;
 };
 map.addControl(attributionControl);
